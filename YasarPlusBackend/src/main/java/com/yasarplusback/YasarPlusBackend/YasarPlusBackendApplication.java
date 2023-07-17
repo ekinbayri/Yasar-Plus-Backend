9696309eab03e7f0a1e150b3c9dfcb8e1e213c02
ekinbayri <ekinbayri@gmail.com>
2023-07-17 16:18:31 +0300

    First Commit

---
+++ YasarPlusBackend/src/main/java/com/yasarplusback/YasarPlusBackend/YasarPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yasarplusback.YasarPlusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasarPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasarPlusBackendApplication.class, args);
+	}
+
+}
---
+++ YasarPlusBackend/src/test/java/com/yasarplusback/YasarPlusBackend/YasarPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasarplusback.YasarPlusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasarPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
